71295c80efd8137063d731ca121aa02e99f04c39
rafayet <78610949+awwmbCPmM9Q7xFfM@users.noreply.github.com>
2021-12-28 00:11:06 -0500

    Update suuuus.py

--- suuuus.py
+++ suuuus.py
@@ -1,29 +1,40 @@
-# shit python bot
-
 import discord
-from discord.ext import commands
-from discord.utils import get
+from discord.commands import Option
+import random
+import time
 
-token = 'dadadadaododod dee"
-client = discord.Client()
+bot = discord.Bot()
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Ready!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.channel.name == "fard-chain":
-        if str(message.content) != 'fard' and str(message.content) != 'Fard':
-            if message.content != "":
-                await client.get_channel(878255368025812993).send(message.content)
-            await message.delete()
+@bot.event
+async def on_message(ctx):
+    if ctx.channel.id == 867074509063323728:
+        if ctx.author.id == 877973364797481011:
+            return
+        if (ctx.content != 'Fard' and ctx.content != 'fard'):
+            await ctx.delete()
+            await bot.get_channel(878255368025812993).send(f'<@{ctx.author.id}> said: "{ctx.content}" in #fard-chain')
+@bot.event
+async def on_message_edit(then, now):
+    if now.channel.id == 867074509063323728:
+        if now.author.id == 877973364797481011:
+            return
+        if (now.content != 'Fard' and now.content != 'fard'):
+            await now.delete()
+            await bot.get_channel(878255368025812993).send(f'<@{now.author.id}> edited chat from: "{then.content}" \n to: "{now.content}" in #fard-chain')
 
-client.run(token)
+@bot.slash_command(name='ping', description="give ping of bot", guild_ids=[866820748366053396])
+async def ping(ctx):
+    before = time.monotonic()
+    message = await ctx.send("ping is:")
+    ping = (time.monotonic() - before) * 1000
+    await message.edit(content=f"ping is:  `{int(ping)}ms`")
 
-#     if message.content.startswith('give mod pls334245'):
-#         member = ctx.message.author
-#         role = get(member.server.roles, name="Test")
-#         await bot.add_roles(member, role)
+@bot.slash_command(name='rng',description="generate a random number", guild_ids=[866820748366053396])
+async def rng(ctx,min:Option(int,'minimum number default=0',required=False,default=0) ,max:Option(int,'maximum number default=10',required=False,default=10)):
+    await ctx.send(f'num: `{random.randrange(min,max)}`')
+  
+bot.run(open('token').read())
